338ec2246427a332e75c40896a9974fd088945ca
unknown <nafis@trigvent.com>
2026-01-08 13:26:44 +0530

    basic tracking features

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import Projects from './components/Project'
 // import Skill from './components/Skill'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import { Analytics } from "@vercel/analytics/react"
+
 
 function App() {
 
